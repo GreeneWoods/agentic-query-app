7fc9241973fcdbe4ab15ca8e2e4bb98707323b1c
Ben <greeneb1123@gmail.com>
2025-09-26 21:32:37 -0400

    this seems all tied up

--- leads_api/src/run_server.py
+++ leads_api/src/run_server.py
@@ -4,4 +4,4 @@ import uvicorn
 
 if __name__ == '__main__':
     app = create_app()
-    uvicorn.run(app, host='localhost', port=8001)
+    uvicorn.run(app, host='0.0.0.0', port=8001)
--- leads_mcp/src/run_server.py
+++ leads_mcp/src/run_server.py
@@ -4,4 +4,4 @@ import uvicorn
 
 if __name__ == '__main__':
     app = create_app()
-    uvicorn.run(app, host='localhost', port=8002)
+    uvicorn.run(app, host='0.0.0.0', port=8002)
